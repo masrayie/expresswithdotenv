4340477b81ba1867b6726b3118519db75fafe5c8
Rayiemas M Putra <rayiemasmputra@Rayiemass-MacBook-Pro.local>
2022-12-11 17:36:57 +0700

    finalize code

--- features/step-definitions/steps.js
+++ features/step-definitions/steps.js
@@ -19,33 +19,31 @@ When(/^send GET request to "([^"]*)"$/, async function (url) {
     let res = await got.get(url)
     let json = JSON.parse(res.body)
     resStatusCode1 = res.statusCode
+    console.log('SCENARIO 1 - STEP 1 - OUTPUT -> GET SUCCESS')
 });
 Then('getEmployee result the status code should be {int}', function (expectedResponse) {
-    // console.log('halo',resStatusCode)
     assert.equal(resStatusCode1, expectedResponse)
+    console.log('SCENARIO 1 - STEP 2 - OUTPUT ->', resStatusCode1)
 });
 
 /// Scenario 2 ////
 When(/^send POST request to "([^"]*)", the data is$/, async function (url, docString) {
-    console.log('hei',docString)
     var bodyData = JSON.parse(docString)
     var data = {
         headers: { 'Content-Type': 'application/json' },
-        // json: docString,
         body: JSON.stringify(bodyData)
     }
-    console.log('coy', data)
     try {
         let res = await got.post(url, data)
         resStatusCode2 = res.statusCode
-        console.log('hayia', resStatusCode2)
+        console.log('SCENARIO 2 - STEP 1 - OUTPUT -> POST SUCCESS')
     } catch (e) {
         console.log('error', e)
     }
 })
 Then('insertEmployee result the status code should be {int}', function (expectedResponse) {
-    console.log('halo',resStatusCode2)
     assert.equal(resStatusCode2, expectedResponse)
+    console.log('SCENARIO 2 - STEP 2 - OUTPUT ->', resStatusCode2)
 })
 
 /// Scenario 3 ///
@@ -53,11 +51,11 @@ When(/^send GET request with employeeId to "([^"]*)"$/, async function (url) {
     let res = await got.get(url)
     let dataResp= JSON.parse(res.body)
     employeeName = dataResp.employeeName
-    console.log('nama', employeeName)
+    console.log('SCENARIO 3 - STEP 1 - OUTPUT -> GET SUCCESS')
 });
 Then('the employeeName should be {string}', function (expectedResponse) {
-    console.log('empName', employeeName)
     assert.equal(employeeName, expectedResponse)
+    console.log('SCENARIO 3 - STEP 2 - OUTPUT ->', employeeName)
 });
 
 /// END ///
